7a5b8060ec5ee5aef486dfb127095422bb8ef6ff
taebinjjang <taebinjjang@naver.com>
2020-07-19 11:27:06 +0900

    test_1

---
+++ BRS_CSM/Assets/Scenes/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vec = this.gameObject.transform.position;
+        if(Input.GetKey(KeyCode.W)) {
+            float flt_x = Random.Range (-50.0f, 50.0f);
+            float flt_y = Random.Range (-50.0f, 50.0f);
+            this.gameObject.transform.position = new Vector3 (flt_x, vec.y, flt_y);
+		}
+    }
+}
